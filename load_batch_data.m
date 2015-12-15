72ba837ae3aeee7514eefeaca956bfa8c10de4f2
sciencefans <sciencefans@foxmail.com>
2015-12-15 20:40:34 +0800

    Add gray_augmentation

--- load_batch_data.m
+++ load_batch_data.m
@@ -30,6 +30,13 @@ end
 % else
 %     train_batch_labels(setdiff(1:num_per_batch, errid)) = label_train(train_batch_id(setdiff(1:num_per_batch, errid)))-1;
 % end
+
+if isfield(para, 'gray_augment_ratio') && para.gray_augment_ratio > 0
+    id_t = find(rand(para.num_per_batch, 1) > para.gray_augment_ratio ~= 0);
+    for i_t = 1 : numel(id_t)
+        train_batch_imgs(:,:,:,id_t(i_t)) = repmat(rgb2gray(train_batch_imgs(:,:,:,id_t(i_t)), [1 1 3]));
+    end
+end
 for i = 1:n_gpu
     dataid = ((i-1)*batch_per_gpu+1):i*batch_per_gpu;
     train_batch{i}{2} = single(zeros(1,1,1,para.num_per_gpu));
