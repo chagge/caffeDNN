5f4131f17093b43d62708533d6f89e9e8cd8d29a
sciencefans <sciencefans@foxmail.com>
2015-12-21 20:07:31 +0800

    Add data augment

--- load_batch_data.m
+++ load_batch_data.m
@@ -1,4 +1,7 @@
 function train_batch = load_batch_data(list_train, rect_train, label_train, meanmat, para)
+	if isfield(para, 'augment') && para.augment ~= 0
+		rect = rect + randi(para.augment*2, size(rect)) - para.augment;
+	end
     if para.pre_load_data
         train_batch = load_batch_data_from_mat(list_train, rect_train, label_train, meanmat, para);
     else
