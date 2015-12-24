b2a97660599bd40c830fc7fac5b9305ce14b29ce
sciencefans <sciencefans@foxmail.com>
2015-12-25 06:25:29 +0800

    bug fixed for augment

--- load_batch_data.m
+++ load_batch_data.m
@@ -1,6 +1,6 @@
 function train_batch = load_batch_data(list_train, rect_train, label_train, meanmat, para)
 	if isfield(para, 'augment') && para.augment ~= 0
-		rect = rect + randi(para.augment*2, size(rect)) - para.augment;
+		rect_train = rect_train + randi(para.augment*2, size(rect_train)) - para.augment;
 	end
     if para.pre_load_data
         train_batch = load_batch_data_from_mat(list_train, rect_train, label_train, meanmat, para);
